309b758a8fb40aff3eb3dbb67c6a067c5f070074
Guilherme <gui.sousa96@gmail.com>
2016-11-20 04:48:33 +0000

    Instalação do sistema RBAC

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,20 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+        ],
+    ],
     'components' => [
+        'authManager' => [
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'mdm\admin\models\User',
+            'loginUrl' => ['admin/user/login'],
         ],
     ],
 ];
