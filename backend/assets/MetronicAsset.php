010aa02c622d906bbf7007b28eb56c8bfd2bb62e
Guilherme <gui.sousa96@gmail.com>
2016-11-21 02:05:53 +0000

    Criei a classe dos assets do Metronic

---
+++ backend/assets/MetronicAsset.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+class MetronicAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+    ];
+    public $js = [
+    ];
+    public $depends = [
+    ];
+}
