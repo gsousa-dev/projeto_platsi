48aff4f0d8e6bf26f3560345c366dc2f42edfee1
Guilherme <gui.sousa96@gmail.com>
2016-11-22 17:52:03 +0000

    Alterei para o layout do yii2 advanced

--- common/config/main.php
+++ common/config/main.php
@@ -2,11 +2,13 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'modules' => [
+        /*
         'admin' => [
             'class' => 'mdm\admin\Module',
-            'layout' => 'right-menu', //'left-menu', //'top-menu', //'@app/views/layouts/user_login',
+            'layout' => 'right-menu', //'left-menu', //'top-menu',
             'mainLayout' => '@app/views/layouts/main.php',
         ],
+        */
     ],
     'components' => [
         'authManager' => [
@@ -16,8 +18,11 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
         'user' => [
+            'identityClass' => 'common\models\User',
+            /*
             'identityClass' => 'mdm\admin\models\User',
             'loginUrl' => ['admin/user/login'],
+            */
         ],
     ],
 ];
