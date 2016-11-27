79ca910d8ff4a6b8334b4ab0d9a5efeda6f96154
Guilherme <gui.sousa96@gmail.com>
2016-11-27 19:13:07 +0000

    Adicionei uma página para o erro 404

--- backend/views/site/error.php
+++ backend/views/site/error.php
@@ -9,19 +9,35 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<div class="breadcrumbs">
+    <ol class="breadcrumb">
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Pages</a>
+        </li>
+        <li class="active">System</li>
+    </ol>
+</div>
+<div class="row">
+    <div class="col-md-12 page-404">
+        <div class="number font-green"> 404 </div>
+        <div class="details">
+            <h3>Oops! You're lost.</h3>
+            <p> We can not find the page you're looking for.<br/>
+                <a href="index.html"> Return home </a> or try the search bar below.
+            </p>
+            <form action="#">
+                <div class="input-group input-medium">
+                    <input type="text" class="form-control" placeholder="keyword...">
+                    <span class="input-group-btn">
+                        <button type="submit" class="btn green">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </form>
+        </div>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
 </div>
