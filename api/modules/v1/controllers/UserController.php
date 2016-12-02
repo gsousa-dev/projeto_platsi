d1652cfcf8e64142db0f151ea9fc28c26d52e060
Guilherme <gui.sousa96@gmail.com>
2016-12-02 15:13:45 +0000

    RESTful API - first commit

---
+++ api/modules/v1/controllers/UserController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use yii\rest\ActiveController;
+
+class UserController extends ActiveController
+{
+    public $modelClass = 'common\models\User';
+}
