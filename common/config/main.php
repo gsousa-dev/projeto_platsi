e59a92c3fee7f6615ead1ff9196917f056db60ac
Guilherme <gui.sousa96@gmail.com>
2016-12-12 16:18:54 +0000

    layout fix

--- common/config/main.php
+++ common/config/main.php
@@ -1,23 +1,28 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'bootstrap' => ['debug'],
     'modules' => [
-            'admin' => [
-                'class' => 'mdm\admin\Module',
-                'layout' => 'right-menu', //'left-menu', //'top-menu',
-                'mainLayout' => '@app/views/layouts/main.php',
+        'debug' => [
+            'class' => 'yii\debug\Module',
+        ],
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+            'layout' => 'right-menu', //'left-menu', //'top-menu',
+            'mainLayout' => '@vendor/mdmsoft/yii2-admin/views/layouts/main.php',
         ],
     ],
     'components' => [
         'authManager' => [
-            'class' => 'yii\rbac\DbManager', // or use 'yii\rbac\PhpManager'
+            'class' => 'yii\rbac\DbManager',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
         'user' => [
-            'identityClass' => 'mdm\admin\models\User',
+            'identityClass' => 'common\models\User',
             'loginUrl' => ['admin/user/login'],
+            //'loginUrl' => ['site/login'],
         ],
     ],
 ];
