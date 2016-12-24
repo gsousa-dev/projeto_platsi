0ea7c8611cf6e6422bc8806323728b93d0ce4b82
Guilherme <gui.sousa96@gmail.com>
2016-12-24 16:35:59 +0000

    API Fotos de Progresso
    A funcionar, com a possibilidade de filtrar por utilizador. Funciona da
    mesma maneira que as pesagens. É necessário o token 'USER-ID'. URL:
    http://localhost/projeto_platsi/api/web/v1/foto-de-progresso/filter-by-user

---
+++ api/modules/v1/controllers/FotoDeProgressoController.php
@@ -0,0 +1,44 @@
+<?php
+namespace api\modules\v1\controllers;
+
+use Yii;
+use yii\rest\ActiveController;
+use yii\filters\Cors;
+//-
+use yii\web\UnauthorizedHttpException;
+//-
+use common\models\FotoDeProgresso;
+//-
+use api\filters\RequestAuthorization;
+
+class FotoDeProgressoController extends ActiveController
+{
+    public $modelClass = 'common\models\FotoDeProgresso';
+
+    /**
+     * @return array
+     */
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        unset($behaviors['authenticator']);
+
+        $behaviors['corsFilter'] = ['class' => Cors::className()];
+        $behaviors['authenticator'] = [
+            'class' => RequestAuthorization::className(),
+        ];
+
+        return $behaviors;
+    }
+
+    public function actionFilterByUser()
+    {
+        $user_id = Yii::$app->request->getHeaders()->get('USER-ID');
+
+        if (empty($user_id)) {
+            throw new UnauthorizedHttpException('Missing user id');
+        }
+
+        return FotoDeProgresso::find()->where(['idCliente' => $user_id])->all();
+    }
+}
