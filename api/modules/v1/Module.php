e2efa3865ac0876e1bd670630105ad72b27a1fcf
Guilherme <gui.sousa96@gmail.com>
2017-01-03 04:51:22 +0000

    Minor change

--- api/modules/v1/Module.php
+++ api/modules/v1/Module.php
@@ -4,7 +4,7 @@ namespace api\modules\v1;
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'api\modules\v1\controllers';
-    
+
     public function init()
     {
         parent::init();
