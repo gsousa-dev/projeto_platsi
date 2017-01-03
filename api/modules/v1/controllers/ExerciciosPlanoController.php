5c2bf4122ae5983dd630eda71a4c4191b3aa6406
Guilherme <gui.sousa96@gmail.com>
2017-01-03 02:52:27 +0000

    ExerciciosPlano API Update

--- api/modules/v1/controllers/ExerciciosPlanoController.php
+++ api/modules/v1/controllers/ExerciciosPlanoController.php
@@ -20,9 +20,6 @@ class ExerciciosPlanoController extends ActiveController
 {
     public $modelClass = 'common\models\ExerciciosPlano';
 
-    /**
-     * @return array
-     */
     public function behaviors()
     {
         $behaviors = parent::behaviors();
@@ -37,54 +34,16 @@ class ExerciciosPlanoController extends ActiveController
         return $behaviors;
     }
 
-    /**
-     * @return array
-     * @throws NotFoundHttpException
-     */
-    public function actionRoutine ()
-    {
-        $plan_id = Yii::$app->request->getHeaders()->get('PLAN-ID'); //Plano que o cliente escolheu
-
-        if (!($exercicios_plano = ExerciciosPlano::find()->where(['idPlano' => $plan_id])->all())) {
-            throw new NotFoundHttpException('Invalid plan.');
-        }
-
-        foreach ($exercicios_plano as $exercicio_plano)
-        {
-            $exercicio = Exercicio::findOne(['idExercicio' => $exercicio_plano->idExercicio]); //Objeto do tipo Exercicio
-            if ($exercicio->tipo_exercicio == 1) {
-                $exercicio_aerobico = ExercicioAerobicoPlano::findOne(['idExercicio' => $exercicio_plano->idExercicio_plano]); //Objeto do tipo ExercicioAerobicoPlano
-                $duracao = $exercicio_aerobico->duracao;
-                $reps = -1;
-                $series = -1;
-            } else if ($exercicio->tipo_exercicio == 2) {
-                $exercicio_anaerobico = ExercicioAnaerobicoPlano::findOne(['idExercicio' => $exercicio_plano->idExercicio_plano]); //Objeto do tipo ExercicioAnaerobicoPlano
-                $duracao = -1;
-                $series = $exercicio_anaerobico->series;
-                $reps = $exercicio_anaerobico->repeticoes;
-            }
-
-            $result[] = [
-                'idExercicio_plano' => $exercicio_plano->idExercicio_plano,
-                'descricao' => $exercicio->descricao,
-                'duracao' => $duracao,
-                'series' => $series,
-                'repeticoes' => $reps
-            ];
-            ArrayHelper::multisort($result, ['idExercicio_plano'], [SORT_ASC]);
-        }
-
-        return $result;
-    }
-
     public function actionRoutines()
     {
         $user_id = Yii::$app->request->getHeaders()->get('USER-ID');
-        $planos = PlanoPessoal::find()->where(['idCliente' => $user_id])->all();
+        if (!($planos = PlanoPessoal::find()->where(['idCliente' => $user_id])->all())) {
+            throw new NotFoundHttpException('Invalid user.');
+        }
 
         foreach ($planos as $plano)
         {
-            $exercicios_plano = ExerciciosPlano::find()->where(['idPlano' => $plano->idPlano])->all(); //exercicios do plano em que o for esta
+            $exercicios_plano = ExerciciosPlano::find()->where(['idPlano' => $plano->idPlano])->all();
 
             foreach ($exercicios_plano as $exercicio_plano)
             {
