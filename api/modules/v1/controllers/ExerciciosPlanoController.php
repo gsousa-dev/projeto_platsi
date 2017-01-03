9751a164ece0831f661db9858bbeaeb99df81200
Guilherme <gui.sousa96@gmail.com>
2017-01-03 01:14:43 +0000

    ExerciciosPlano API Update
    URL:
    http://localhost/projeto_platsi/api/web/v1/exercicios-plano/routines
    Precisa do token USER-ID

--- api/modules/v1/controllers/ExerciciosPlanoController.php
+++ api/modules/v1/controllers/ExerciciosPlanoController.php
@@ -8,6 +8,7 @@ use yii\helpers\ArrayHelper;
 //-
 use yii\web\NotFoundHttpException;
 //-
+use common\models\PlanoPessoal;
 use common\models\ExerciciosPlano;
 use common\models\ExercicioAerobicoPlano;
 use common\models\ExercicioAnaerobicoPlano;
@@ -72,6 +73,46 @@ class ExerciciosPlanoController extends ActiveController
             ];
             ArrayHelper::multisort($result, ['idExercicio_plano'], [SORT_ASC]);
         }
+
+        return $result;
+    }
+
+    public function actionRoutines()
+    {
+        $user_id = Yii::$app->request->getHeaders()->get('USER-ID');
+        $planos = PlanoPessoal::find()->where(['idCliente' => $user_id])->all();
+
+        foreach ($planos as $plano)
+        {
+            $exercicios_plano = ExerciciosPlano::find()->where(['idPlano' => $plano->idPlano])->all(); //exercicios do plano em que o for esta
+
+            foreach ($exercicios_plano as $exercicio_plano)
+            {
+                $exercicio = Exercicio::findOne(['idExercicio' => $exercicio_plano->idExercicio]);
+                if ($exercicio->tipo_exercicio == 1) {
+                    $exercicio_aerobico = ExercicioAerobicoPlano::findOne(['idExercicio' => $exercicio_plano->idExercicio_plano]); //Objeto do tipo ExercicioAerobicoPlano
+                    $duracao = $exercicio_aerobico->duracao;
+                    $reps = -1;
+                    $series = -1;
+                } else if ($exercicio->tipo_exercicio == 2) {
+                    $exercicio_anaerobico = ExercicioAnaerobicoPlano::findOne(['idExercicio' => $exercicio_plano->idExercicio_plano]); //Objeto do tipo ExercicioAnaerobicoPlano
+                    $duracao = -1;
+                    $series = $exercicio_anaerobico->series;
+                    $reps = $exercicio_anaerobico->repeticoes;
+                }
+
+                $result[] = [
+                    'idPlano' => $plano->idPlano,
+                    'idExercicio_plano' => $exercicio_plano->idExercicio_plano,
+                    'descricao' => $exercicio->descricao,
+                    'duracao' => $duracao,
+                    'series' => $series,
+                    'repeticoes' => $reps
+                ];
+                ArrayHelper::multisort($result, ['idExercicio_plano'], [SORT_ASC]);
+            }
+        }
+
         return $result;
     }
 }
